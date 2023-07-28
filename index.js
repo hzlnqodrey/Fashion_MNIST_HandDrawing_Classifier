9774e3b178df6605021eff2810a56cc5f0ad4ddb
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-07-28 18:29:58 +0700

    add mnist function into one html and modify a code a lil bit, and add the model solution and weight

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.use(express.static(path.join(__dirname, '4 - MNIST_drawing_classifier_CNN')))
 
